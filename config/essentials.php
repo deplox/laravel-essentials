438d7ce5b5d98e18401df967dde659bf1841fcef
Jona Goldman <jonagoldman@gmail.com>
2026-02-22 23:23:25 +0000

    refactor: rename remaining packages from jonagoldman/ to deplox/
    Rename jonagoldman/laravel-support to deplox/laravel-support (namespace
    Deplox\Support) and jonagoldman/laravel-essentials to
    deplox/laravel-essentials (namespace Deplox\Essentials). All three
    monorepo packages now live under packages/deplox/ with consistent
    Deplox\ namespaces.

---
+++ config/essentials.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'fake_sleep' => true,
+    'prevent_stray_requests' => true,
+    'force_https' => true,
+    'aggressive_prefetching' => true,
+    'immutable_dates' => true,
+    'unguard_model' => false,
+    'strict_model' => true,
+    'automatic_eager_load_relationships' => true,
+    'require_morph_map' => false,
+    'prohibit_destructive_commands' => true,
+    'set_default_passwords' => true,
+    'default_string_length' => 255,
+    'default_morph_key_type' => 'int',
+];
